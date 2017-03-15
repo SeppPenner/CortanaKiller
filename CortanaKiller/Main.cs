8844bb00e33caf71a83dc1ea7b5a6b7fafc83c33
tim <jan-brenner@gmx.de>
2017-03-15 17:26:22 +0100

    Initial commit.

---
+++ CortanaKiller/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+public partial class Main : Form
+{
+    public Main()
+    {
+        InitializeComponent();
+        Visible = false;
+        while (true)
+            try
+            {
+                Process.GetProcesses()
+                    .Where(x => x.ProcessName.ToLower().Contains("searchui"))
+                    .ToList()
+                    .ForEach(x => x.Kill());
+            }
+            catch
+            {
+                // ignored
+            }
+        // ReSharper disable once FunctionNeverReturns
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+CortanaKiller
+====================================
+
+CortanaKiller is an executable to permanently kill the Cortana Background Task in Windows 10.
+The executable was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/1k7itaajt66mxgc7?svg=true)](https://ci.appveyor.com/project/SeppPenner/cortanakiller)
+
+
+Change history
+--------------
+
+* **Version 1.0.0.1 (2017-03-15)** : Switched to .Net to 4.6.2, Refactored code.
+* **Version 1.0.0.0 (2017-03-15)** : 1.0 release.
