0614cd4ffcf7346db5aae98a730e535bcef7b550
SeppPenner <HansM2013@gmx.de>
2022-02-10 19:17:00 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

---
+++ src/CortanaKiller/GlobalUsings.cs
@@ -0,0 +1,3 @@
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
+global using System.Diagnostics;
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
--- src/CortanaKiller/Main.cs
+++ src/CortanaKiller/Main.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Main.cs" company="Hämmer Electronics">
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,39 +7,32 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace CortanaKiller
+namespace CortanaKiller;
+
+/// <summary>
+/// The main form.
+/// </summary>
+public partial class Main : Form
 {
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Windows.Forms;
-
     /// <summary>
-    /// The main form.
+    /// Initializes a new instance of the <see cref="Main"/> class.
     /// </summary>
-    public partial class Main : Form
+    public Main()
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Main"/> class.
-        /// </summary>
-        public Main()
+        this.InitializeComponent();
+        this.Visible = false;
+
+        while (true)
         {
-            this.InitializeComponent();
-            this.Visible = false;
-
-            while (true)
+            try
             {
-                try
-                {
-                    Process.GetProcesses().Where(x => x.ProcessName.ToLower().Contains("searchui")).ToList()
-                        .ForEach(x => x.Kill());
-                }
-                catch
-                {
-                    // ignored
-                }
+                Process.GetProcesses().Where(x => x.ProcessName.ToLower().Contains("searchui")).ToList()
+                    .ForEach(x => x.Kill());
             }
-
-            // ReSharper disable once FunctionNeverReturns
+            catch
+            {
+                // ignored
+            }
         }
     }
 }
